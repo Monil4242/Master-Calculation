17d74db29364022729f9d47f058c872a8f9a6017
monil sojitra <monilsojitra42@gmail.com>
2023-10-25 14:25:33 +0530

    Initial Commit

---
+++ Master Calculation/viewControler/aboutPage.swift
@@ -0,0 +1,28 @@
+
+import UIKit
+
+class aboutPage: UIViewController {
+    
+    @IBOutlet weak var shareBtn: UIButton!
+    @IBOutlet weak var rateBtn: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        shareBtn.layer.borderWidth = 1
+        shareBtn.layer.borderColor = UIColor.white.cgColor
+        shareBtn.layer.cornerRadius = 10
+        
+        rateBtn.layer.borderWidth = 1
+        rateBtn.layer.borderColor = UIColor.white.cgColor
+        rateBtn.layer.cornerRadius = 10
+
+    }
+    
+    @IBAction func backBtn(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    
+
+}
